b3e1bfd45cf8bb87c303a3fb40941e9ac6002327
Zoe Weinstein <mxzweinstein@gmail.com>
2021-11-27 15:50:12 -0700

    initial commit with sidebar

---
+++ README.md
@@ -0,0 +1,5 @@
+take input from color picker and show it in a display div
+make a sidebar with a new div
+save to that new div
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("rSideBar").style.width = "30%";
+  //document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("rSideBar").style.width = "0";
+ // document.getElementById("main").style.marginLeft = "0";
+}
+
