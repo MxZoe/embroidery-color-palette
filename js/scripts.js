e9fcb9d9819a0de623337a623147e12b8c2ce8d9
Zoe Weinstein <mxzweinstein@gmail.com>
2021-11-27 17:28:58 -0700

    add save button function

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,65 @@
-function openNav() {
-  document.getElementById("rSideBar").style.width = "30%";
-  //document.getElementById("main").style.marginLeft = "250px";
+//functional logic
+function openBar() {
+  document.getElementById("rSideBar").style.width = "35%";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-function closeNav() {
+function closeBar() {
   document.getElementById("rSideBar").style.width = "0";
- // document.getElementById("main").style.marginLeft = "0";
 }
 
+function makeColorID(counter){
+  return "#saveColor" + counter;
+}
+
+function saveColor(counter, currentColor){
+  let currentColorID = makeColorID(counter);
+  $(currentColorID).css("background-color", currentColor);
+}
+
+function addSaveDiv(counter){
+  let divName = "<div class='col-sm-3 colorDiv' id='saveColor" + counter +"'></div>";
+  return divName;
+}
+//Color Object business logic
+function Color(hex){
+  this.hex = hex;
+}
+
+function createColor(hex){
+  let newColor = new Color(hex);
+  return newColor;
+}
+
+//UI logic
+$(document).ready(function(){
+  let sidebarDisplay = false;
+  let display = $("#colorDisplay");
+  let colorArray = [];
+  let counter = 0;
+
+  $("#color-picker").on("input", function(){
+    let currentHex = $("#color-picker").val();
+    $(display).css("background-color", currentHex);
+  });
+  $("#toggleButton").click(function(event){
+    if(sidebarDisplay){
+      closeBar();
+      sidebarDisplay = false;
+    } else{
+      openBar();
+      sidebarDisplay = true;
+    }
+    event.preventDefault();
+  });
+
+  $("#saveButton").click(function(event){
+    let currentHex = $("#color-picker").val();
+    let newColor = createColor(currentHex);
+    colorArray.push(newColor);
+    let newSaveDiv = addSaveDiv(counter);
+    $("#colorDisplay").after(newSaveDiv);
+    saveColor(counter, currentHex);
+
+    event.preventDefault();
+  })
+})
